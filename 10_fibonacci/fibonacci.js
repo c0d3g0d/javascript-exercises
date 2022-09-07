6dccbf7921d154795cc9541134bfe586a6487e94
Shahzad Karim <karimshahzad67@yahoo.com>
2022-09-07 17:34:12 +0500

    Solve fibonacci recursively

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n, a = 1, b = 1) {
+    if (n == 1)
+        return a;
+    else if (n <= 0)
+        return 'OOPS';
+        
+    return fibonacci(n - 1, b, a + b);
 };
 
 // Do not edit below this line
